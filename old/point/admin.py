e81e9942bdcb7f74d58d1fa453aeb2813516607f
mysteryfuko <mysteryfuko@foxmail.com>
2020-08-19 17:12:12 +0800

    remove old code

--- old/point/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import score,record,user,xiaohao
-
-admin.site.register(score)
-admin.site.register(record)
-admin.site.register(user)
-admin.site.register(xiaohao)
